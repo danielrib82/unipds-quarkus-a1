020c16f4f291bc4c3e080682b8c0a6eca4f89599
Daniel Vasconcelos <danielrib82@hotmail.com>
2025-11-14 02:34:31 -0300

    Criado Resources para expor EndPoints e Service para consumo de API externa

---
+++ src/main/java/br/com/d13solutions/ApplicationDefault.java
@@ -0,0 +1,8 @@
+package br.com.d13solutions;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class ApplicationDefault extends Application {
+}
